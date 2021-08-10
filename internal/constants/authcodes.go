b5722b6b0818aaf4904d7ba2233d3617dc7f6fbd
Quentin McGaw (desktop) <quentin.mcgaw@gmail.com>
2021-08-10 09:54:14 -0400

    Maint: remove auth codes psql logic

--- internal/constants/authcodes.go
+++
@@ -1,9 +0,0 @@
-package constants
-
-const (
-	AuthCodeMinutes                      = 4
-	AuthCodeCreationsPerPeriod           = 1
-	AuthCodeCreationRateLimitMinutes     = 1
-	AuthCodeVerificationsPerPeriod       = 3
-	AuthCodeVerificationRateLimitMinutes = 3
-)
